4b3f1242d3dd3127d9420dd68ae97ba49eaeb29b
arahnidous <arahnidous@gmail.com>
2014-01-31 09:20:58 +0000

    Upload main script. First edition.

---
+++ geteset.php
@@ -0,0 +1,152 @@
+#!/usr/local/bin/php
+<?php
+$settings = parse_ini_file("settings.txt");
+
+$tempeset = "/var/eset/";
+
+if (!is_dir($settings['upload_dir'])){
+        mkdir($settings['upload_dir']);
+}
+if (!is_dir($tempeset)){
+        mkdir($tempeset);
+}
+
+function getostype(){
+        exec("/usr/bin/uname -o", $res);
+        preg_match('/BSD/i', $res[0], $ostype);
+        if(isset($ostype[0])){
+                if($ostype[0] == "BSD"){
+                        $ospath = "local/";
+                } else {
+                        $ospath = "";
+                }
+        return $ospath;
+        } else {
+                return 0;
+        }
+}
+passthru("/usr/".getostype()."bin/wget -c http://update.eset.com/eset_upd/v".$settings['ver']."/update.ver -O ".$tempeset."update.rar", $result);
+passthru("/usr/".getostype()."bin/unrar e -y ".$tempeset."update.rar ".$tempeset, $result);
+passthru("/bin/rm ".$settings['upload_dir']."/*", $result);
+$upfile = file_get_contents($tempeset.'update.ver');
+$serv_list = explode('[HOSTS]', $upfile);
+$serv_list = explode('[Expire]', $serv_list[1]);
+preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $serv_list[0], $servers);
+$ns = count($servers[0]);
+
+$srv = array();
+
+for ($i=0;$i<$ns;$i++){
+	$srv[] = parse_url($servers[0][$i], PHP_URL_HOST);
+}
+$strngfile = explode("\r\n", $upfile);
+
+$updfiles = array();
+$filenames = array();
+
+$lines = count($strngfile);
+for ($j=0;$j<$lines;$j++){
+	preg_match('/^file=/i', $strngfile[$j], $result);	
+	if (isset($result[0]) and $result[0] == 'file='){
+		$nfile = preg_replace('/^file=/i', '', $strngfile[$j]);
+		$nnfile = end(explode("/",$nfile));
+		$updfiles[] = $nfile;
+		$filenames[] = $nnfile;
+	}
+
+		
+}
+$numfiles = count($updfiles);
+for ($k=1;$k<$numfiles;$k++){
+		getfilefromweb($updfiles[$k]);
+}
+
+function getfilefromweb($gf){
+	global $srv, $settings;
+	$ns = count($srv);
+	for ($i=0;$i<$ns;$i++){
+	$link = "http://".$srv[$i];	
+	$state = get_headers($link);
+	preg_match('/403 Forbidden/i', $state[0], $searchhttp);
+        if ($searchhttp[0] == "403 Forbidden"){
+		$param = $settings['upload_dir']." --http-user=".$settings['user']." --http-password=".$settings['password']." ".$link.$gf;
+		passthru("/usr/".getostype()."bin/wget -P ".$param, $result);		
+		return 0;		
+	}	
+	
+	}
+}
+
+// Check filelist
+// ===========================================
+$getdir = scandir($settings['upload_dir']);
+$dirfilenames = array();
+$cgd = count($getdir);
+for($n=0;$n<$cgd;$n++){
+	if($getdir[$n]!="." and $getdir[$n]!=".."){
+		$dirfilenames[] = $getdir[$n];
+	}
+}
+$checkfiles = array_diff($filenames, $dirfilenames);
+$cchecked = count($checkfiles);
+$msg = "";
+$sign = "";
+$err_array = array();
+if($cchecked>0){
+	for($m=0;$m<$cchecked;$m++){
+		if(isset($checkfiles[$m]) and $checkfiles[$m]!="update.ver" and $checkfiles[$m] != ""){
+			$err_array[] = $checkfiles[$m];	
+		}
+	}
+
+}
+if(count($err_array)>0){
+	$msg .= "Error of downloading files! ";
+	$msg .= count($err_array)." files not downloading!";
+	$sign = "1";
+	}
+ 	else {
+		$msg .= "Updating complete!";
+		$sign = 0;
+ 	}
+
+switch($settings['log_type'])
+{
+	case "file":
+		file_put_contents($settings['log_file'], date("Y-m-d H:i:s").": ".$msg."\n",FILE_APPEND);
+        break;
+	case "db":
+        	if(isset($settings['log_db_table'])){
+			$con=mysqli_connect($settings['log_db_host'],$settings['log_db_user'],$settings['log_db_password'],$settings['log_db_base']);
+			if (mysqli_connect_errno())
+  				{
+  				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  				}
+			mysqli_query($con,"INSERT INTO ".$settings['log_db_table']." (`date`, `sign`, `log`) VALUES ('".date("Y-m-d H:i:s")."','".$sign."','".$msg."')");
+			mysqli_close($con);
+		}
+	break;
+	default:
+		echo "";
+}
+
+// ============================================
+$upfile2 = file_get_contents($tempeset.'update.ver');
+$pie = explode('[PCUVER]', $upfile2);
+$strfile = explode("\r\n", $pie[1]);
+$nl = count($strfile);
+for ($l=3;$l<$nl;$l++){
+	preg_match('/^file=/i', $strfile[$l], $result);
+
+        if (isset($result[0]) and $result[0] == 'file='){
+        	$nline = preg_replace('/^file=/i', '', $strfile[$l]);
+                $nnline = end(explode("/",$nline));
+                $strfile[$l] = "file=".$nnline;
+	}
+	file_put_contents($settings['upload_dir']."/update.ver", $strfile[$l]."\n",FILE_APPEND);
+}
+// --------------------------------------------
+passthru("/bin/rm ".$tempeset."update.rar", $result);
+passthru("/bin/rm ".$tempeset."update.ver", $result);
+echo "\nAll temp files is deleted...\n"
+?>
